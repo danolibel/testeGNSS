575d514f8e63a94f1fc30835441af95b4868372d
unknown <Administrador@LYNXBHZANB0084.tsa.local>
2023-08-21 23:16:23 -0300

    commit

--- GNSSLogger/app/src/main/java/com/google/android/apps/location/gps/gnsslogger/data/LoginDataSource.java
+++ GNSSLogger/app/src/main/java/com/google/android/apps/location/gps/gnsslogger/data/LoginDataSource.java
@@ -12,7 +12,7 @@ import java.io.IOException;
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource  implements HttpPost.OnResponseReceivedListener{
-    static boolean loginToken = false;
+    static boolean loginToken = true;
 
     public Result<LoggedInUser> login(String username, String password) {
 
@@ -42,8 +42,8 @@ public class LoginDataSource  implements HttpPost.OnResponseReceivedListener{
             Log.println(Log.INFO,"1","1");
            LoggedInUser fakeUser =
                     new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            username);
+                    java.util.UUID.randomUUID().toString(),
+                    username);
             Log.println(Log.INFO,"1","1");
 
             if (loginToken){
@@ -51,7 +51,7 @@ public class LoginDataSource  implements HttpPost.OnResponseReceivedListener{
             }
             else{
 
-                return new aResult.Error(new IOException("Error logging in"));
+                return new Result.Error(new IOException("Error logging in"));
             }
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
